81062db1ceb9e25ac561fdc2f08a46b95bb9b080
Luan Mascarenhas <58894199+luanmas@users.noreply.github.com>
2023-12-08 05:44:04 -0300

    Update library.h

--- library.h
+++ library.h
@@ -1,21 +1,10 @@
-/*Aluno: Luan Silva Mascarenhas
-Matricula: 00000000000
-Aluno: Isaac Nycolas
-Matricula: 00000000000
-Aluno: Breno Pinheiro
-Matricula: 00000000000
-Avaliação 04: Trabalho Final
-Compilador: gcc Versão: version 6.3.0 (MinGW.org GCC-6.3.0-1)
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
 
-struct pgm
-{
-	int tipo;
+struct pgm{
+	int tipo; // 8 ou 16
 	int c;
 	int r;
 	int mv;
