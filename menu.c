a4eeaa3b4bb6c57f2962c62d9359b628d88ebaeb
Luan Mascarenhas <58894199+luanmas@users.noreply.github.com>
2023-12-08 05:44:34 -0300

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,39 +1,22 @@
-/*Aluno: Mikaelle Costa dos Santos
-Matricula: 20221045050311
-Aluno: Ana Flávia Torres do Carmo
-Matricula: 20221045050427
-Aluno: Isabelly Pinheiro da Costa
-Matricula: 20221045050460
-Aluno: Larissa do Nascimento Vieira
-Matricula: 20202045050412
-Avaliação 04: Trabalho Final
-04.505.23 - 2022.2 - Prof. Daniel Ferreira
-Compilador: gcc Versão: version 6.3.0 (MinGW.org GCC-6.3.0-1)
-*/
-
 #include "library.h"
 
-int menu()
-{
+int menu(){
   int op;
-
+  
   puts("\nQuantizar imagens em:\n");
   puts("\t(1) 8 níveis");
   puts("\t(2) 16 níveis");
   printf("\nOpção: ");
-  scanf("%d", &op);
+  scanf("%d",&op);
   puts("");
 
-  switch (op)
-  {
-  case 1:
-    return 8;
-    break;
-  case 2:
-    return 16;
-    break;
-  default:
-    puts("Opção Inválida!");
-    exit(1);
+  switch(op){
+    case 1:
+      return 8;
+    case 2:
+      return 16;
+    default:
+      puts("Opção Inválida!");
+      exit(1);
   }
 }
