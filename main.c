ced4195f34d43f7a4583ba9f6f239ad1eb29b1d8
Luan Mascarenhas <58894199+luanmas@users.noreply.github.com>
2023-12-08 05:43:33 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,55 +1,42 @@
-/*Aluno: Luan Silva Mascarenhas
-Matricula: 00000000000
-Aluno: Isaac Nycolas
-Matricula: 00000000000
-Aluno: Breno Pinheiro
-Matricula: 00000000000
-Avaliação 04: Trabalho Final
-Compilador: gcc Versão: version 6.3.0 (MinGW.org GCC-6.3.0-1)
-*/
 #include <dirent.h>
 #include "library.h"
 
-#define QTDIMG 1312
+// #define QTDIMG 1312
+// #define FOLDER "./oncotex_pgm"
 #define FOLDER "./oncotex_pgm"
-
-int main()
-{
-
-  clock_t begin, end;
-  double time_per_img, time_total = 0;
-
+ 
+int main(){
+  
+	clock_t begin, end;
+	double time_per_img, time_total=0;
+  
   DIR *d;
   struct dirent *dir;
   d = opendir(FOLDER);
 
   FILE *fp;
 
-  // Abre um arquivo pra ordem dos arquivos lidos
-  if (!(fp = fopen("matrixOrder.txt", "a+")))
-  {
-    perror("Erro.");
-    exit(1);
-  }
+  //Abre um arquivo pra ordem dos arquivos lidos
+  if (!(fp = fopen("matrixOrder.txt","a+"))){
+		perror("Erro.");
+		exit(1);
+	}
 
-  if (d)
-  {
+  if (d){
 
-    // Menu quantização
+    //Menu quantização
     int level = menu();
     printf("Nivel: %d\n\n", level);
-
-    while (((dir = readdir(d)) != NULL))
-    {
+    
+    
+    while (((dir = readdir(d)) != NULL)){
 
       // Ignorar arquivo "." e ".."
-      if (!strcmp(dir->d_name, ".") || !strcmp(dir->d_name, ".."))
-      {
+      if (!strcmp(dir->d_name, ".") || !strcmp(dir->d_name,"..")){
         continue;
       }
 
-      if (strstr(dir->d_name, "mean"))
-      {
+      if(strstr(dir->d_name,"mean")){
         continue;
       }
 
@@ -59,47 +46,46 @@ int main()
       struct pgm img1, img2;
 
       // Leitura da Imagem -PGM
-      readPGMImage(&img1, FOLDER, dir->d_name);
-
+      readPGMImage(&img1,FOLDER,dir->d_name);
+      
       // Quantização da Imagem -PGM
-      quantize(&img1, level);
-
+      quantize(&img1,level);
+      
       // dir = readdir(d); // Próximo arquivo
 
-      for (int i = 0; i < strlen(dir->d_name); i++)
-      {
-        if (dir->d_name[i] == '.')
-        {
-          dir->d_name[i] = '\0';
+      for(int i=0;i<strlen(dir->d_name);i++){
+        if(dir->d_name[i] == '.') {
+          dir->d_name[i]='\0';
           fprintf(fp, "%s\n", dir->d_name);
         }
       }
 
-      strcat(dir->d_name, "_mean.pgm");
-
+      strcat(dir->d_name,"_mean.pgm");
+      
       // Leitura da Imagem suavizada -PGM
-      readPGMImage(&img2, FOLDER, dir->d_name);
-
+      readPGMImage(&img2,FOLDER,dir->d_name);
+      // Filtrar a imagem 
       // Quantização da Imagem suavizada -PGM
-      quantize(&img2, level);
-
+      quantize(&img2,level);
+      
       // Saida - Salvar matriz vetorizada no arquivo de características com o rotulo no final
       SCM(&img1, &img2, dir->d_name, level);
+      
 
       // Finalizar medição do tempo.
       end = clock();
 
       // Acumular tempo
       time_per_img = (double)(end - begin) / CLOCKS_PER_SEC;
-      time_total += time_per_img;
+		  time_total += time_per_img;
     }
     fclose(fp);
     closedir(d);
-
+    
     // Calcular tempo médio por imagem.
-    printf("\nTempo médio: %lf\n", time_total / (QTDIMG / 2));
-    printf("Tempo Total: %lf\n", time_total);
+    // printf("\nTempo médio: %lf\n",time_total/(QTDIMG/2));
+    printf("Tempo Total: %lf\n",time_total);
   }
 
-  return (0);
+  return(0);
 }
